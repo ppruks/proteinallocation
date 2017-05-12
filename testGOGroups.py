da03410ccf91c145715b7d3f696d40c83e8d1f33
ppruks <ppruks@alumni.stanford.edu>
2017-05-12 14:57:26 +0300

    add test to GOGroups

---
+++ testGOGroups.py
@@ -0,0 +1,15 @@
+from GOGroups import GOGroups
+from matplotlib import pyplot as plt
+from matplotlib_venn import venn2
+
+def plotVenn(first, second):
+	venn2(subsets = (first.getProteins(), other.getProteins()), set_labels = (first.getName(), other.getName()))
+	plt.show()
+
+filename1 = "gogroups/go006417.txt"
+filename2 = "gogroups/go0006555.txt"
+ribosome = GOGroups(filename1, "ribosome")
+other = GOGroups(filename2, "other")
+plotVenn(ribosome, other)
+ribosome.keepSelf(other)
+plotVenn(ribosome, other)
